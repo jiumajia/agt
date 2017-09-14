0802e7e4c57e077d91c81b6a706334f44e15ee32
miaoluzeng@exingcai.com <miaoluzeng@exingcai.com>
2017-09-14 10:04:19 +0800

    first

---
+++ agt/AGT001/insertion_sort.py
@@ -0,0 +1,14 @@
+import random
+# insertion sort T=O(n^2)
+#
+def insert_sort(*arg)
+    
+
+    return 
+
+if __name__ ==  "__main__":
+    unsorted_list = [random.random() for _ in range(10)]
+    print unsorted_list
+    sorted_list = insert_sort(unsorted_list)
+    print sorted_list
+    
