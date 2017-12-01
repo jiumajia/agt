70aa0df500eac79a6f4dd9dcafb5716f7e60b113
miaoluzeng <miaoluzeng@100.me>
2017-12-01 15:23:30 +0800

    update python3

--- agt/AGT001/insertion_sort.py
+++ agt/AGT001/insertion_sort.py
@@ -1,14 +1,35 @@
 import random
+
 # insertion sort T=O(n^2)
+# origin = [4,5,2,7]
+# init : origin = [4,5,2,7] new = [4]
+#        origin = [4,->5,2,7] new = [4,5]
+#        origin = [4,5,->2,7] new = [2,4,5]
+#        origin = [4,5,2,->7] new = [2,4,5,7]
 #
-def insert_sort(*arg)
-    
+# update with python3
 
-    return 
 
-if __name__ ==  "__main__":
-    unsorted_list = [random.random() for _ in range(10)]
-    print unsorted_list
+def insert_sort(unsorted_list):
+
+    new_list = [unsorted_list[0]]
+    for num in unsorted_list[1:]:
+        flag = 0
+        for index, position in enumerate(new_list):
+            if num <= position:
+                flag = 1
+                break
+        if flag:
+            new_list.insert(index, num)
+        else:
+            new_list.append(num)
+
+    return new_list
+
+
+if __name__ == "__main__":
+    unsorted_list = [random.randint(1, 10) for _ in range(10)]
+    print(unsorted_list)
     sorted_list = insert_sort(unsorted_list)
-    print sorted_list
+    print(sorted_list)
     
