55b5ce0f910c1766c0a71abdcc5dcde3b4843914
miaoluzeng <miaoluzeng@100.me>
2017-12-01 21:15:51 +0800

    update python3

---
+++ agt/AGT001/selection_sort.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8 -*-
+
+import random
+
+# PYTHON  T=O(n^2)
+#
+#
+
+def selection_sort(unsorted_list):
+    pass
+
+
+if __name__ == '__main__':
+    unsorted_list = [random.randint(1, 10) for _ in range(10)]
+    selection_sort()
